98e05a77a5390f77849e758a8c2b17d9fb32a69f
MuchiriKinyua <muchirikinyua2000@gmail.com>
2024-12-20 15:00:38 +0300

    script English to Russian

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const selectTag = document.querySelectorAll("select");
 
-selectTag.forEach(tag => {
+selectTag.forEach((tag, id) => {
     for (const country_code in countries) {
-        console.log(country_code)
+        let selected;
+        if(id == 0 && country_code == "en-GB"){
+            selected = "selected";
+        } else if(id == 1 && country_code == "ru-RU"){     
+            selected = "selected";
+        }
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option); 
     }
 });
